6e49aaeb5b4b4cd8341735d50bd4d4ee1dc6d512
joaqperalta <lahim.kane@outlook.com>
2020-10-14 17:12:55 +0530

    wip

--- NodeServer/src/express-config.js
+++ NodeServer/src/express-config.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const gqlServer = require('./graphql/graphqlServer');
 require('dotenv').config();
@@ -14,7 +15,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 gqlServer.applyMiddleware({ app });
 
 module.exports = app;
